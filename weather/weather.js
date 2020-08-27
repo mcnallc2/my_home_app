a8c6f5e8e4cb2eaa0beaa6f6d7b0b41fa6784b6d
mcnallc2 <mcnallc2@tcd.ie>
2020-08-27 20:46:33 +0100

    adding weather API

---
+++ weather/weather.js
@@ -0,0 +1,43 @@
+
+//  Conor McNally - 16325133
+//  News Headlines
+//  Server
+
+"use strict";
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+//this allows my client to communicate with this server with cors enabled
+app.use(cors())
+
+const port = 3002
+app.get('/weather/:location', news)
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
+
+function news(req, res) {
+
+    let loc = req.params.location;
+
+    console.log("Request for News Data")
+
+    var request = require("request");
+
+    var options = {
+        method: 'GET',
+        url: 'https://community-open-weather-map.p.rapidapi.com/forecast',
+        qs: {q: loc},
+        headers: {
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+          'x-rapidapi-key': '0dda254da1mshfaf81c9032a7c2ep1f2122jsn019ecc0cc376',
+          useQueryString: true
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+        res.json(body)
+    });
+}
