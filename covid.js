73a736185ddf3c8c72423ff7403e31d5368fb615
mcnallc2 <mcnallc2@tcd.ie>
2020-08-27 20:28:38 +0100

    adding covid API

---
+++ covid.js
@@ -0,0 +1,48 @@
+
+//  Conor McNally - 16325133
+//  Covid19
+//  Server
+
+"use strict";
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+//this allows my client to communicate with this server with cors enabled
+app.use(cors())
+
+const port = 3001
+app.get('/covid/:location', covid)
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
+
+function covid(req, res) {
+    let location = req.params.location;
+    console.log("Request for Covid Data")
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+
+    var request = require("request");
+
+    var options = {
+    method: 'GET',
+    url: 'https://covid-19-data.p.rapidapi.com/country',
+    qs: {'date-format': 'YYYY-MM-DD', format: 'json', date: today, name: location},
+    headers: {
+        'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
+        'x-rapidapi-key': '0dda254da1mshfaf81c9032a7c2ep1f2122jsn019ecc0cc376',
+        useQueryString: true
+    }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+        res.json(body)
+    });
+}
